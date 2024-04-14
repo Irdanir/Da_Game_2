a3038c46513ccc4acc2dc26fe654f118006e0e92
Nikita Stepanov <mdebvisitor@mail.ru>
2024-04-14 20:23:29 +0300

    bullets disappear

---
+++ core/src/com/mygdx/game/MenuScreen.java
@@ -0,0 +1,63 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.World;
+
+import helper.TileMapHelper;
+
+public class MenuScreen implements Screen {
+    private OrthographicCamera camera;
+    private Batch batch;
+    private World world;
+    private Box2DDebugRenderer box2DDebugRenderer;
+    private OrthogonalTiledMapRenderer orthogonalTiledMapRenderer;
+    private TileMapHelper tileMapHelper;
+    public MenuScreen (OrthographicCamera camera) {
+        this.camera = camera;
+        this.batch = new SpriteBatch();
+        this.world = new World(new Vector2(0, -9.81f), false);
+        this.box2DDebugRenderer = new Box2DDebugRenderer();
+        //this.tileMapHelper = new TileMapHelper(this);
+        this.orthogonalTiledMapRenderer = tileMapHelper.setupMap();
+    }
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
